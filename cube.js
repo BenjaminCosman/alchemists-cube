6540e64f1f1406a51b25d39f46560fa6dcf16750
Rafael Cosman <rafaelcosman@gmail.com>
2016-11-06 14:44:36 -0800

    adds silver and gold circles to corners

--- cube.js
+++ cube.js
@@ -1,8 +1,11 @@
 hues = [0, 120, 240]
+
 for (hueIndex in hues) {
   hue = hues[hueIndex]
   thisFace = 'hsl(' + hue + ', 100%, 50%)'
   otherFace = 'hsl(' + hue+120 + ', 100%, 50%)'
+  gold = 'gold'
+  silver = 'silver'
 
   draw = SVG('cube' + hueIndex)
   draw.size(100, 100)
@@ -10,7 +13,7 @@ for (hueIndex in hues) {
   var height = 100
 
   var gradient = draw.gradient('linear', function(stop) {
-    stop.at(0, thisFace)
+    stop.at(0, 'hsl(' + hue + ', 100%, 80%)')
     stop.at(1, '#FFFFFF')
   })
 
@@ -22,4 +25,10 @@ for (hueIndex in hues) {
 
   draw.line(0, 0, 0, height).stroke({width: 10, color: otherFace})
   draw.line(width, 0, width, height).stroke({width: 10, color: otherFace})
+
+  diameter = 50
+  draw.circle(diameter).center(0, 0).fill(gold)
+  draw.circle(diameter).center(width, 0).fill(silver)
+  draw.circle(diameter).center(0, height).fill(silver)
+  draw.circle(diameter).center(width, height).fill(gold)
 }
