504820cba8c81165a2ddc69245e48425bd90a7fa
Rafael Cosman <rafaelcosman@gmail.com>
2016-11-06 15:01:38 -0800

    breaks into 6 svgs

--- cube.js
+++ cube.js
@@ -1,34 +1,47 @@
 hues = [0, 120, 240]
 
 for (hueIndex in hues) {
-  hue = hues[hueIndex]
-  thisFace = 'hsl(' + hue + ', 100%, 50%)'
-  otherFace = 'hsl(' + hue+120 + ', 100%, 50%)'
-  gold = 'gold'
-  silver = 'silver'
+  for (sign in [0, 1]) {
+      if (sign == 0)
+        signText = '-'
+      else
+        signText = '+'
 
-  draw = SVG('cube' + hueIndex)
-  draw.size(100, 100)
-  var width = 100
-  var height = 100
+    hue = hues[hueIndex]
+    thisFace = 'hsl(' + hue + ', 100%, 50%)'
+    otherFace = 'hsl(' + hue+120 + ', 100%, 50%)'
+    gold = 'gold'
+    silver = 'silver'
 
-  var gradient = draw.gradient('linear', function(stop) {
-    stop.at(0, 'hsl(' + hue + ', 100%, 80%)')
-    stop.at(1, '#FFFFFF')
-  })
+    draw = SVG('cube' + hueIndex + signText)
+    var width = 300
+    var height = width
+    draw.size(width, height)
 
-  draw.rect(100, 100).fill(gradient)
-  draw.line(0, 0, width, height).stroke({width: 10, color: thisFace})
-  draw.line(0, 0, width, 0).stroke({width: 10, color: thisFace})
-  draw.line(0, height, width, height).stroke({width: 10, color: thisFace})
-  draw.line(0, height, width, 0).stroke({width: 10, color: thisFace})
+    var gradient = draw.gradient('linear', function(stop) {
+      stop.at(0, 'hsl(' + hue + ', 100%, 80%)')
+      stop.at(1, '#FFFFFF')
+    })
 
-  draw.line(0, 0, 0, height).stroke({width: 10, color: otherFace})
-  draw.line(width, 0, width, height).stroke({width: 10, color: otherFace})
+    draw.rect(width, height).fill(gradient)
+    draw.line(0, 0, width, height).stroke({width: 10, color: thisFace})
+    draw.line(0, 0, width, 0).stroke({width: 10, color: thisFace})
+    draw.line(0, height, width, height).stroke({width: 10, color: thisFace})
+    draw.line(0, height, width, 0).stroke({width: 10, color: thisFace})
 
-  diameter = 50
-  draw.circle(diameter).center(0, 0).fill(gold)
-  draw.circle(diameter).center(width, 0).fill(silver)
-  draw.circle(diameter).center(0, height).fill(silver)
-  draw.circle(diameter).center(width, height).fill(gold)
+    draw.line(0, 0, 0, height).stroke({width: 10, color: otherFace})
+    draw.line(width, 0, width, height).stroke({width: 10, color: otherFace})
+
+    diameter = 50
+    draw.circle(diameter).center(0, 0).fill(gold)
+    draw.circle(diameter).center(width, 0).fill(silver)
+    draw.circle(diameter).center(0, height).fill(silver)
+    draw.circle(diameter).center(width, height).fill(gold)
+
+    margin = 10
+    draw.text(signText).font({size: 12}).center(0+margin, 0+margin)
+    draw.text(signText).font({size: 12}).center(width-margin, 0+margin)
+    draw.text(signText).font({size: 12}).center(0+margin, height-margin)
+    draw.text(signText).font({size: 12}).center(width-margin, height-margin)
+  }
 }
