22f86561ca33712def60afaf42ef4a8e51b491aa
Rafael Cosman <rafaelcosman@gmail.com>
2016-11-06 14:34:08 -0800

    adds multiple cube sides

--- cube.js
+++ cube.js
@@ -1,12 +1,25 @@
-draw = SVG('cube')
-width = 100
-height = 100
-var gradient = draw.gradient('linear', function(stop) {
-  stop.at(0, '#ffffff')
-  stop.at(1, '#0000ff')
-})
-draw.rect(100, 100).fill(gradient)
-draw.line(0, 0, width, height).stroke({width: 10})
-draw.line(0, 0, width, 0).stroke({width: 10})
-draw.line(0, height, width, height).stroke({width: 10})
-draw.line(0, height, width, 0).stroke({width: 10})
+hues = [0, 120, 240]
+for (hueIndex in hues) {
+  hue = hues[hueIndex]
+  thisFace = 'hsl(' + hue + ', 100%, 50%)'
+  otherFace = 'hsl(' + hue+120 + ', 100%, 50%)'
+
+  draw = SVG('cube' + hueIndex)
+  draw.size(100, 100)
+  var width = 100
+  var height = 100
+
+  var gradient = draw.gradient('linear', function(stop) {
+    stop.at(0, thisFace)
+    stop.at(1, '#FFFFFF')
+  })
+
+  draw.rect(100, 100).fill(gradient)
+  draw.line(0, 0, width, height).stroke({width: 10, color: thisFace})
+  draw.line(0, 0, width, 0).stroke({width: 10, color: thisFace})
+  draw.line(0, height, width, height).stroke({width: 10, color: thisFace})
+  draw.line(0, height, width, 0).stroke({width: 10, color: thisFace})
+
+  draw.line(0, 0, 0, height).stroke({width: 10, color: otherFace})
+  draw.line(width, 0, width, height).stroke({width: 10, color: otherFace})
+}
