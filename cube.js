293a0b83b3e2c4a63598855d4f856738750b167b
Rafael Cosman <rafaelcosman@gmail.com>
2016-11-06 14:17:37 -0800

    adds hourglass

--- cube.js
+++ cube.js
@@ -1,1 +1,7 @@
 draw = SVG('cube')
+width = 100
+height = 100
+draw.line(0, 0, width, height).stroke({width: 10})
+draw.line(0, 0, width, 0).stroke({width: 10})
+draw.line(0, height, width, height).stroke({width: 10})
+draw.line(0, height, width, 0).stroke({width: 10})
